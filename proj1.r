a8eabf296f2b1dd0da87d2b1cc2d7478548bf434
Yue913 <145402616+Yue913@users.noreply.github.com>
2023-10-06 11:39:11 +0100

    Update proj1.r

--- proj1.r
+++ proj1.r
@@ -172,7 +172,7 @@ for(iteration in 1:48){
 }
 
 cat <- b[kab] ## "kab" is the word index corresponding to common words, b[kab] gives the final 50-words section.
-print(cat)
+paste(cat, sep=" ") ## shows the final 50-words section.
 
 
 ## step 9
@@ -180,7 +180,7 @@ print(cat)
 freq_b <- sorted_tabulate[1:mword]
 prob_b <- freq_b/sum(tabulate(index_vector))
 text_comparison <- sample(b,50,prob=prob_b) ## "sample" function independently selects 50 characters from b with probability vector specified by frequencies
-print(text_comparison)
+paste(text_comparison, sep=" ") ## shows the final 50-words section.
 
 
 ## step10
@@ -200,4 +200,4 @@ b_cap <- which(cap_prob>=0.5) ## determine the first letter of which word should
 B_cap <- b ## create auxiliary variable, in order to make a modified version of b while not affecting b
 substr(B_cap[b_cap],1,1) <- toupper(substr(B_cap[b_cap],1,1)) ## modify b so that the first letters of selected words are transformed into capital ones
 cat_after_cap <- B_cap[kab] ## update generating sentence in step 8
-print(cat_after_cap)
+paste(cat_after_cap, sep=" ") ## shows the final 50 words section after updating.
